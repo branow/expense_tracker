1870754ad9e0b421db5516d329e0d283e084b1c6
branow <bodnarorestdraw@gmail.com>
2024-02-16 12:43:31 +0200

    modify TransactionService: clean structure

--- src/main/java/com/upwork/expense_tracker/service/TransactionService.java
+++ src/main/java/com/upwork/expense_tracker/service/TransactionService.java
@@ -1,112 +1,59 @@
 package com.upwork.expense_tracker.service;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
 import com.upwork.expense_tracker.dto.TransactionCreatingRequest;
+import com.upwork.expense_tracker.dto.TransactionResponse;
 import com.upwork.expense_tracker.dto.TransactionUpdatingRequest;
+import com.upwork.expense_tracker.entity.Transaction;
 import com.upwork.expense_tracker.exception.EntityNotFoundException;
 import com.upwork.expense_tracker.mapper.TransactionMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.upwork.expense_tracker.constant.Messages;
-import com.upwork.expense_tracker.entity.Transaction;
 import com.upwork.expense_tracker.repository.TransactionRepository;
 import com.upwork.expense_tracker.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class TransactionService {
 
-    @Autowired
-    TransactionRepository transactionRepository;
-
-    @Autowired
+    TransactionRepository repository;
     UserRepository userRepository;
-
-    @Autowired
-    TokenUtility tokenUtility;
-
-    @Autowired
     TransactionMapper mapper;
 
-    public List<String> createTransaction(TransactionCreatingRequest dto, String token) {
-        if (token == null) {
-            return Arrays.asList(Messages.EMPTY_TOKEN);
-        }
 
-        if (!tokenUtility.validateToken(token)) {
-            return Arrays.asList(Messages.INVALID_TOKEN);
-        }
-        String userName = tokenUtility.extractUsername(token);
-
-        Integer userId = userRepository.findIdByEmail(userName);
-
+    public TransactionResponse create(TransactionCreatingRequest dto, String userEmail) {
+        Integer userId = userRepository.findIdByEmail(userEmail);
         Transaction transaction = mapper.toTransaction(dto);
         transaction.setUserId(userId);
         transaction.setDate(new Date());
-        transactionRepository.save(transaction);
-        return Arrays.asList(Messages.SUCCESS);
+        return mapper.toTransactionResponse(repository.save(transaction));
     }
 
-    public List<String> updateTransaction(TransactionUpdatingRequest transaction, String token) {
-
-        Optional<Transaction> optionalTransaction;
-        if (token == null) {
-            return Arrays.asList(Messages.EMPTY_TOKEN);
-        }
-
-        if (!tokenUtility.validateToken(token)) {
-            return Arrays.asList(Messages.INVALID_TOKEN);
-        }
-
-        String userName = tokenUtility.extractUsername(token);
-
-        optionalTransaction = transactionRepository.findByTransactionIdUserId(transaction.getId(), userRepository.findIdByEmail(userName));
-
-        Transaction getTransaction = optionalTransaction.orElseThrow(() ->
-                new EntityNotFoundException(Transaction.class, "id", transaction.getId()));
-
-        getTransaction.setType(transaction.getType());
-        getTransaction.setDescription(transaction.getDescription());
-
-        transactionRepository.save(getTransaction);
-        return Arrays.asList(Messages.SUCCESS);
+    public Transaction get(Integer transactionId) {
+        return repository.findById(transactionId)
+                .orElseThrow(() -> new EntityNotFoundException(Transaction.class, "id", transactionId));
+    }
+    public List<TransactionResponse> getAll(String userEmail) {
+        Integer userId = userRepository.findIdByEmail(userEmail);
+        return repository.findByUserId(userId).stream()
+                .map(mapper::toTransactionResponse)
+                .toList();
     }
 
-    public List<String> deleteTransaction(Integer id, String token) {
-
-        if (token == null) {
-            return Arrays.asList(Messages.EMPTY_TOKEN);
-        }
-
-        if (!tokenUtility.validateToken(token)) {
-            return Arrays.asList(Messages.INVALID_TOKEN);
-        }
-
-        if (id == null) {
-            return Arrays.asList(Messages.EMPTY_TRANSACTION_ID);
-        }
-
-        transactionRepository.deleteById(id);
-        return Arrays.asList(Messages.DELETED);
+    public TransactionResponse update(TransactionUpdatingRequest transaction) {
+        Transaction old = get(transaction.getId());
+        old.setType(transaction.getType());
+        old.setDescription(transaction.getDescription());
+        return mapper.toTransactionResponse(repository.save(old));
     }
 
-    public Object getTransactions(String token) {
-
-        if (token == null) {
-            return Arrays.asList(Messages.EMPTY_TOKEN);
-        }
-
-        if (!tokenUtility.validateToken(token)) {
-            return Arrays.asList(Messages.INVALID_TOKEN);
-        }
-        String userName = tokenUtility.extractUsername(token);
-
-        Integer userId = userRepository.findIdByEmail(userName);
-
-        return transactionRepository.findByUserId(userId);
+    public void delete(Integer id) {
+        repository.deleteById(id);
     }
+
 }
